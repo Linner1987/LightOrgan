03cd49b3c095ed3566209e6627d0ceac2c673f3c
Marcin <martin.kruszynski@gmail.com>
2016-03-23 00:23:42 +0100

    file list search

--- Android/app/src/main/java/com/apps/kruszyn/lightorganapp/FileListActivity.java
+++ Android/app/src/main/java/com/apps/kruszyn/lightorganapp/FileListActivity.java
@@ -1,28 +1,38 @@
 package com.apps.kruszyn.lightorganapp;
 
 import android.content.Context;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
 import android.text.format.DateUtils;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class FileListActivity extends AppCompatActivity {
+public class FileListActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
 
     private RecyclerView mRecyclerView;
-    private RecyclerView.Adapter mAdapter;
+    private List<MediaFileItem> mModel;
+    private SimpleItemRecyclerViewAdapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_file_list);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         mRecyclerView = (RecyclerView) findViewById(R.id.item_list);
 
@@ -31,14 +41,89 @@ public class FileListActivity extends AppCompatActivity {
         mLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
 
-        mAdapter = new SimpleItemRecyclerViewAdapter(SampleData.MEDIA_FILE_ITEMS);
+        mModel = SampleData.MEDIA_FILE_ITEMS;
+        mAdapter = new SimpleItemRecyclerViewAdapter(mModel);
         mRecyclerView.setAdapter(mAdapter);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_file_list, menu);
+
+        final MenuItem item = menu.findItem(R.id.action_search);
+        final SearchView searchView = (SearchView) MenuItemCompat.getActionView(item);
+        searchView.setMaxWidth(Integer.MAX_VALUE);
+        searchView.setQueryHint("Search songs");
+        searchView.setOnQueryTextListener(this);
+
+        MenuItemCompat.setOnActionExpandListener(item,
+                new MenuItemCompat.OnActionExpandListener() {
+                    @Override
+                    public boolean onMenuItemActionCollapse(MenuItem item) {
+                        mAdapter.setFilter(mModel);
+                        return true;
+                    }
+
+                    @Override
+                    public boolean onMenuItemActionExpand(MenuItem item) {
+                        return true;
+                    }
+                });
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        switch (item.getItemId()) {
+            case R.id.action_search:
+
+
+
+                return true;
+
+            case R.id.action_settings:
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        final List<MediaFileItem> filteredList = filter(mModel, newText);
+        mAdapter.setFilter(filteredList);
+        return true;
+    }
+
+    private List<MediaFileItem> filter(List<MediaFileItem> items, String query) {
+        query = query.toLowerCase();
+
+        final List<MediaFileItem> filteredList = new ArrayList<>();
+        for (MediaFileItem item : items) {
+            final String text = item.name.toLowerCase();
+            if (text.contains(query)) {
+                filteredList.add(item);
+            }
+        }
+        return filteredList;
+    }
+
 
     public class SimpleItemRecyclerViewAdapter
             extends RecyclerView.Adapter<SimpleItemRecyclerViewAdapter.ViewHolder> {
 
-        private final List<MediaFileItem> mValues;
+        private List<MediaFileItem> mValues;
 
         public SimpleItemRecyclerViewAdapter(List<MediaFileItem> items) {
             mValues = items;
@@ -72,6 +157,11 @@ public class FileListActivity extends AppCompatActivity {
         @Override
         public int getItemCount() {
             return mValues.size();
+        }
+
+        public void setFilter(List<MediaFileItem> items){
+            mValues = items;
+            notifyDataSetChanged();
         }
 
         public class ViewHolder extends RecyclerView.ViewHolder {
