01a421bb191978628ef74e86b435844741868abd
MarcinKruszynski <martin.kruszynski@gmail>
2016-04-29 08:21:04 +0200

    iOS Swift Xcode first commit

---
+++ iOS/LightOrganApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LightOrganApp
+//
+//  Created by Marcin Kruszyński on 29/04/16.
+//  Copyright © 2016 Marcin Kruszyński. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
