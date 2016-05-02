426fdf2ef8b7e0874c784c8f4e530f5b6ea4f80a
MarcinKruszynski <martin.kruszynski@gmail>
2016-05-02 15:34:18 +0200

    Media Picker Controller

--- iOS/LightOrganApp/ViewController.swift
+++ iOS/LightOrganApp/ViewController.swift
@@ -7,9 +7,11 @@
 //
 
 import UIKit
+import MediaPlayer
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MPMediaPickerControllerDelegate {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +21,25 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func search(sender: AnyObject) {
+        
+        let picker = MPMediaPickerController(mediaTypes: MPMediaType.Music)
+        picker.delegate = self
+        picker.allowsPickingMultipleItems = true
+        picker.prompt = NSLocalizedString("Select items to play", comment: "Select items to play")
+        self.presentViewController(picker, animated: true, completion: nil)
+    }
+    
+    func mediaPickerDidCancel(mediaPicker: MPMediaPickerController) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func mediaPicker(mediaPicker: MPMediaPickerController, didPickMediaItems mediaItemCollection: MPMediaItemCollection) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+        
+        //to do
+    }
+    
 }
 
