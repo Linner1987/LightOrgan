036cfd54028f1ea25c1af1369ebaa4a62d57aada
MarcinKruszynski <martin.kruszynski@gmail>
2016-05-06 23:57:26 +0200

    black uisearchbar

--- iOS/LightOrganApp/CustomSearchBar.swift
+++ iOS/LightOrganApp/CustomSearchBar.swift
@@ -10,8 +10,38 @@ import UIKit
 
 class CustomSearchBar: UISearchBar {
     
+    
     override func layoutSubviews() {
         super.layoutSubviews()
         setShowsCancelButton(false, animated: false)
+    }
+    
+    
+    func indexOfSearchFieldInSubviews() -> Int! {
+        var index: Int!
+        let searchBarView = subviews[0]
+        
+        for i in 0 ..< searchBarView.subviews.count {
+            if searchBarView.subviews[i].isKindOfClass(UITextField) {
+                index = i
+                break
+            }
+        }
+        
+        return index
+    }
+    
+    
+    override func drawRect(rect: CGRect) {
+        
+        if let index = indexOfSearchFieldInSubviews() {
+            
+            let searchField: UITextField = subviews[0].subviews[index] as! UITextField
+            
+            searchField.textColor = .whiteColor()
+            searchField.backgroundColor = UIColor(red: 0.14, green: 0.14, blue: 0.14, alpha: 1.0)
+        }
+        
+        super.drawRect(rect)
     }
 }
