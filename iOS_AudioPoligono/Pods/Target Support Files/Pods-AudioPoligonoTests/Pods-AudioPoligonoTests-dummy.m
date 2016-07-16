54e8ae0c60a97d44b5458527626fea5fdaeb4a0d
MarcinKruszynski <martin.kruszynski@gmail.com>
2016-07-16 20:44:29 +0200

    -AudioPoligono +Xamarin Android skeleton

--- iOS_AudioPoligono/Pods/Target Support Files/Pods-AudioPoligonoTests/Pods-AudioPoligonoTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_AudioPoligonoTests : NSObject
-@end
-@implementation PodsDummy_Pods_AudioPoligonoTests
-@end
--- iOS_AudioPoligono/Pods/Target Support Files/Pods-AudioPoligonoUITests/Pods-AudioPoligonoUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_AudioPoligonoUITests : NSObject
-@end
-@implementation PodsDummy_Pods_AudioPoligonoUITests
-@end
