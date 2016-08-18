331fb50bd8e51a899031e7f44a8fc06179d314a7
MarcinKruszynski <martin.kruszynski@gmail.com>
2016-08-18 22:42:55 +0200

    Xamarin.iOS:  CircleView

--- Xamarin/LightOrganApp.iOS/CircleView.cs
+++ Xamarin/LightOrganApp.iOS/CircleView.cs
@@ -1,4 +1,5 @@
-﻿using Foundation;
+﻿using CoreGraphics;
+using Foundation;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -10,17 +11,54 @@ namespace LightOrganApp.iOS
     [Register("CircleView"), DesignTimeVisible(true)]
     public class CircleView: UIView
     {
+        private UIColor circleColor;
+
         [Export("CircleColor"), Browsable(true)]
-        public UIColor CircleColor { get; set; }
+        public UIColor CircleColor
+        {
+            get { return circleColor; }
+            set
+            {
+                circleColor = value;
+                SetNeedsDisplay();
+            }
+        }        
 
         public CircleView(IntPtr p): base(p)
         {
-            
+            Initialize();
         }
 
         public CircleView()
         {
-            
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            circleColor = UIColor.Red;
+            ContentMode = UIViewContentMode.Redraw;
+
+            SetNeedsDisplay();
+        }
+
+        public override void Draw(CGRect rect)
+        {
+            DrawCircle(CircleColor);
+        }
+
+        private void DrawCircle(UIColor color)
+        {
+            using (var context = UIGraphics.GetCurrentContext())
+            {
+                var a = Math.Min(Bounds.Size.Width, Bounds.Size.Height);
+                var leftX = Bounds.GetMidX() - a / 2;
+                var topY = Bounds.GetMidY() - a / 2;
+                var rectangle = new CGRect(leftX, topY, a, a);
+
+                context.SetFillColor(CircleColor.CGColor);
+                context.FillEllipseInRect(rectangle);
+            }
         }
     }
 }
