ae22d9cf81db2056d7e7d11adc497d64df5483f6
MarcinKruszynski <martin.kruszynski@gmail.com>
2016-09-04 12:18:42 +0200

    Xamarin.Forms  localization preview

--- Xamarin.Forms/LightOrganApp/LightOrganApp/App.xaml.cs
+++ Xamarin.Forms/LightOrganApp/LightOrganApp/App.xaml.cs
@@ -13,6 +13,15 @@ namespace LightOrganApp
         {
             InitializeComponent();
 
+            // This lookup NOT required for Windows platforms - the Culture will be automatically set
+            if (Device.OS == TargetPlatform.iOS || Device.OS == TargetPlatform.Android)
+            {
+                // determine the correct, supported .NET culture
+                var ci = DependencyService.Get<ILocalize>().GetCurrentCultureInfo();
+                Resx.AppResources.Culture = ci; // set the RESX for resource localization
+                DependencyService.Get<ILocalize>().SetLocale(ci); // set the Thread for locale-aware methods
+            }
+
             MainPage = new LightOrganApp.MainPage();
         }
 
